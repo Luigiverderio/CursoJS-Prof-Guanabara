5b9696e7f994a07619a13f3473d820b071a69f71
Luigi Verdério <luigiverderio@hotmail.com>
2024-03-01 22:48:56 -0300

    Create ambiente.js

---
+++ Aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [8, 0, 6]; // Criando uma variável composta
+
+// TODO : Adicionar um valor adicional ao vetor
+
+num[3] = 12; // Adicionado o número 12 no índice 3 do vetor
+
+
+console.losg(`Nosso vetor é o ${num}`); // Exibindo o conteúdo da array
+
