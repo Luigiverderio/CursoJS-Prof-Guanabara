66bc92bcd520e6bb9481c72dcf3931fc4f986dd4
Luigi Verdério <luigiverderio@hotmail.com>
2024-03-01 23:34:33 -0300

    Adicionando a extensão elsint

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -4,7 +4,6 @@ let num = [8, 0, 6]; // Criando uma variável composta
 
 num[3] = 12; // Adicionado o número 12 no índice 3 do vetor
 
-
 console.log(`Nosso vetor é o ${num}`); // Exibindo o conteúdo da array
 
 console.log(num.length)// Retornando a quantidade de elementos dentro do vetor
@@ -20,4 +19,9 @@ console.log(`O terceiro valor do nosso vetor é ${num[2]}`)
 
 //!Estou usando várias extensões divertidas!
 
+console.warn('Eu sou um aviso')
 
+function soma(a,b){
+  return(a+b)
+}
+console.log(soma(5,4))
