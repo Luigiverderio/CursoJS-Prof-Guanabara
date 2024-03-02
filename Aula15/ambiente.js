e842d1022cd8051fe46b785c5799aa6a13307f6c
Luigi Verdério <luigiverderio@hotmail.com>
2024-03-01 22:53:40 -0300

    Update ambiente.js

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -7,3 +7,8 @@ num[3] = 12; // Adicionado o número 12 no índice 3 do vetor
 
 console.losg(`Nosso vetor é o ${num}`); // Exibindo o conteúdo da array
 
+num.length; // Retornando a quantidade de elementos dentro do vetor
+
+
+
+
